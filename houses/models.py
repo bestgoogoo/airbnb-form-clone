d0938e8aeb5abba100610b4bd6da285d60ad982d
bestgoogoo <junta9996@gmail.com>
2023-07-25 17:22:40 +0900

    Revision someting
    1. Seperate APPS in config/settings.py
    2. Rename price to price_per_night in house/models.py
    3. Add pet_allowed in house/models.py

--- houses/models.py
+++ houses/models.py
@@ -6,6 +6,7 @@ class House(models.Model):
     """Model definition for Houses"""
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pet_allowed = models.BooleanField(default=True)
