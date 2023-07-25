71e6a341741e14d6ed93f8204cbb6bab63ebda8d
bestgoogoo <junta9996@gmail.com>
2023-07-25 17:58:09 +0900

    Move house models options

--- houses/models.py
+++ houses/models.py
@@ -6,10 +6,13 @@ class House(models.Model):
     """Model definition for Houses"""
 
     name = models.CharField(max_length=140)
-    price_per_night = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField(
+        verbose_name="Price", help_text="Enter Only Positive Numbers per Night")
     description = models.TextField()
     address = models.CharField(max_length=140)
-    pet_allowed = models.BooleanField(default=True)
+    pets_allowed = models.BooleanField(
+        verbose_name="Pets Allowed?",
+        default=True, help_text="Dose this house allow pets?")
 
     def __str__(self):
         return self.name
