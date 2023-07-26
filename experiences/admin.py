6eeed2e5060e7784137d50f42fd79880765521d4
bestgoogoo <junta9996@gmail.com>
2023-07-27 01:42:54 +0900

    Create ExperiencesApps
    1. Make experiences model
    2. Make experiences admin

---
+++ experiences/admin.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from .models import Experience, Content
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "host",
+        "created_at",
+    )
+    list_filter = (
+        "country",
+        "city",
+        "price",
+        "host",
+        "start",
+        "end",
+        "contents",
+        "created_at",
+    )
+
+
+@admin.register(Content)
+class ContentAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "detail",
+        "created_at",
+        "updated_at",
+    )
+
+    readonly_fields = (
+        "created_at",
+        "updated_at",
+    )
---
+++ experiences/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from common.models import CommonModel
+
+
+class Experience(CommonModel):
+    name = models.CharField(max_length=250, default="")
+    country = models.CharField(max_length=50, default="Korea")
+    city = models.CharField(max_length=80, default="Seoul")
+    price = models.PositiveIntegerField()
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    address = models.CharField(max_length=250)
+    start = models.TimeField()
+    end = models.TimeField()
+    description = models.TextField()
+    contents = models.ManyToManyField("experiences.Content")
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Content(CommonModel):
+
+    """What is included on an Experience"""
+
+    name = models.CharField(max_length=100)
+    detail = models.CharField(max_length=250, null=True, blank=True)
+    explanation = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name
