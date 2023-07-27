cef4014fc8d6ec6b70de4c583be237467a436763
bestgoogoo <junta9996@gmail.com>
2023-07-27 19:02:01 +0900

    Create ReviewsApp
    1. Make review model
    2. Make review admin

---
+++ reviews/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Review
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "__str__",
+        "room",
+        "experience",
+        "payload",
+    )
+
+    list_filter = (
+        "room",
+        "experience",
+        "rating",
+    )
