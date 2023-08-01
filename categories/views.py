241ccee7d0b2d4983a7aeb218ed2b39104a2f2b6
bestgoogoo <junta9996@gmail.com>
2023-08-01 15:12:48 +0900

    Make Category URL by api_view & Response in rest  - Add category

--- categories/views.py
+++ categories/views.py
@@ -8,9 +8,11 @@ from .serializers import CategorySerializer
 def categories(request):
     all_categories = Category.objects.all()
     serializer = CategorySerializer(all_categories, many=True)
-    return Response(
-        {
-            "OK": True,
-            "categories": serializer.data,
-        },
-    )
+    return Response(serializer.data)
+
+
+@api_view()
+def category(request, pk):
+    category = Category.objects.get(pk=pk)
+    serializer = CategorySerializer(category)
+    return Response(serializer.data)
