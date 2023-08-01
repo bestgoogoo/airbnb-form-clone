6307230d26cc7db0f64f810fcbfcb23c4ce1f43e
bestgoogoo <junta9996@gmail.com>
2023-08-01 14:59:23 +0900

    Fix Error by Serializers in rest  - Serializers are translate QuerySet to JSON

--- categories/views.py
+++ categories/views.py
@@ -1,14 +1,16 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
 
 
 @api_view()
 def categories(request):
     all_categories = Category.objects.all()
+    serializer = CategorySerializer(all_categories, many=True)
     return Response(
         {
             "OK": True,
-            "categories": all_categories,
+            "categories": serializer.data,
         },
     )
