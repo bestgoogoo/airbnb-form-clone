f57612fb1f4d211ad37d82d94e3ad729f8dd58da
bestgoogoo <junta9996@gmail.com>
2023-08-01 14:47:40 +0900

    Make Categories URL by api_view & Response in rest  - Choose this buildup

--- categories/views.py
+++ categories/views.py
@@ -1,13 +1,14 @@
-from django.http import JsonResponse
-from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
 
 
+@api_view()
 def categories(request):
     all_categories = Category.objects.all()
-    return JsonResponse(
+    return Response(
         {
             "OK": True,
-            "categories": serializers.serialize("json", all_categories),
-        }
+            "categories": all_categories,
+        },
     )
