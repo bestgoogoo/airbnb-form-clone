07238a5bb04a92e9288337939c259bcd3538952a
bestgoogoo <junta9996@gmail.com>
2023-08-06 17:47:09 +0900

    Add SerializerMethodField in RoomDetail  - This name is 'is_liked'

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -3,6 +3,7 @@ from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
 from reviews.serializers import ReviewSerializer
 from medias.serializers import PhotoSerializer
+from wishlists.models import Wishlist
 from .models import Amenity, Room
 
 
@@ -18,6 +19,7 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     photos = PhotoSerializer(read_only=True, many=True)
     rating = serializers.SerializerMethodField()
     is_owner = serializers.SerializerMethodField()
+    is_liked = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
@@ -29,6 +31,13 @@ class RoomDetailSerializer(serializers.ModelSerializer):
     def get_is_owner(self, room):
         request = self.context["request"]
         return room.owner == request.user
+
+    def get_is_liked(self, room):
+        request = self.context["request"]
+        return Wishlist.objects.filter(
+            user=request.user,
+            rooms__pk=room.pk,
+        ).exists()
 
 
 class RoomListSerializer(serializers.ModelSerializer):
