9d3692d981c85ca9937980b08fb2de157d52425c
bestgoogoo <junta9996@gmail.com>
2023-08-07 16:35:35 +0900

    About User 1. GET, PUT User profile view  - Serializer takes over checking user update validation 2. POST User view  - Do not use password instead of set_password in save password 3. GET PublicUser view   - URL code have to lower than "me" URL   - Do not "me" URL approach by using '@' 4. PUT ChangePassword view 5. POST LogIn, LogOut view  - Using django.contrib.auth

--- users/views.py
+++ users/views.py
@@ -1,3 +1,102 @@
-from django.shortcuts import render
+from django.contrib import auth
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions, exceptions
+from . import serializers, models
 
-# Create your views here.
+
+class Me(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        serializer = serializers.PrivateUserSerializer(user)
+        return Response(serializer.data)
+
+    def put(self, request):
+        user = request.user
+        serializer = serializers.PrivateUserSerializer(
+            user,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            user = serializer.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class Users(APIView):
+    def post(self, request):
+        password = request.data.get("password")
+        if not password:
+            raise exceptions.ParseError("Enter password")
+        serializer = serializers.PrivateUserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(password)
+            user.save()
+            serializer = serializers.PrivateUserSerializer(user)
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+
+
+class PublicUser(APIView):
+    def get_object(self, username):
+        try:
+            return models.User.objects.get(username=username)
+        except models.User.DoesNotExist:
+            raise exceptions.NotFound
+
+    def get(self, request, username):
+        user = self.get_object(username)
+        serializer = serializers.TinyUserSerializer(user)
+        return Response(serializer.data)
+
+
+class ChangePassword(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def put(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+        if not old_password or not new_password:
+            raise exceptions.ParseError
+        if user.check_password(old_password):
+            if user.check_password(old_password) == user.check_password(new_password):
+                raise exceptions.ParseError("Same as before passwored")
+            user.set_password(new_password)
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class LogIn(APIView):
+    def post(self, request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        if not username or not password:
+            raise exceptions.ParseError
+        user = auth.authenticate(
+            request,
+            username=username,
+            password=password,
+        )
+        if user:
+            auth.login(request, user)
+            return Response({"ok": "Done"})
+        else:
+            return Response({"error": "Wrong password"})
+
+
+class LogOut(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        auth.logout(request)
+        return Response({"bye": "bye"})
