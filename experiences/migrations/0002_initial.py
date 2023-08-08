72e49e1a5c1e30c0653d3fd41c0cb058e505f4cb
bestgoogoo <junta9996@gmail.com>
2023-08-08 17:18:05 +0900

    😂 Data Reset...

--- experiences/migrations/0002_initial.py
+++ experiences/migrations/0002_initial.py
@@ -10,14 +10,14 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('rooms', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('experiences', '0001_initial'),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='room',
-            name='owner',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='rooms', to=settings.AUTH_USER_MODEL),
+            model_name='experience',
+            name='host',
+            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='experiences', to=settings.AUTH_USER_MODEL),
         ),
     ]
