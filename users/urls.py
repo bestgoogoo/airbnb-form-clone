1e856f1e53a167db71147316c9af417330b34cce
bestgoogoo <junta9996@gmail.com>
2023-08-09 16:41:25 +0900

    JWT(Json Web Token) Authentication - Use pyJWT library - token of authToken save in db but token of JWT don`t save in db

--- users/urls.py
+++ users/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path("", views.Users.as_view()),
     path("me", views.Me.as_view()),
     path("change-password", views.ChangePassword.as_view()),
-    path("token-login", obtain_auth_token),
-    path("log-in", views.LogIn.as_view()),
+    path("jwt-login", views.JWTLogIn.as_view()),  # JWT login using username, password
+    path("token-login", obtain_auth_token),  # authToken login using username, password
+    path("log-in", views.LogIn.as_view()),  # cookie login using username, password
     path("log-out", views.LogOut.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
 ]
