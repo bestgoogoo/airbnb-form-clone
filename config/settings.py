2e50f164bb627001be90bb2f683f002550b1e32e
bestgoogoo <junta9996@gmail.com>
2023-08-09 17:35:37 +0900

    Hide SECRET_KEY - Using django-environ for connecting .env to settingsAPP

--- config/settings.py
+++ config/settings.py
@@ -11,16 +11,24 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-2re2xesw64zd%qjhzn-%0r+*-2%hxw9%)clbm5a%x#0cw1g19("
+SECRET_KEY = env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
