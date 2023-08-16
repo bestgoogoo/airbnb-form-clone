5c8b1c4e6be73a06802ed0baaeb2adc8ced87ae7
bestgoogoo <junta9996@gmail.com>
2023-08-16 17:10:49 +0900

    Code(client_id) from frontend - it just check 'code'

--- users/urls.py
+++ users/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path("jwt-login", views.JWTLogIn.as_view()),  # JWT login using username, password
     path("token-login", obtain_auth_token),  # authToken login using username, password
     path("log-in", views.LogIn.as_view()),  # cookie login using username, password
+    path("github", views.GithubLogIn.as_view()),
     path("log-out", views.LogOut.as_view()),
     path("@<str:username>", views.PublicUser.as_view()),
 ]
