21f74ffc6cc78fac8d961c4e01f26abcbaa41672
bestgoogoo <junta9996@gmail.com>
2023-08-23 17:11:35 +0900

    Something Fixed for UploadRoom - should divide category into rooms_category and experiences_category - Reference this code from frontend: https://github.com/bestgoogoo/airbnb-form-clone-frontend/commit/96e727c34e0d97cd8221c0d5dfa7cde7537f938b

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -10,7 +10,11 @@ from .models import Amenity, Room
 class AmenitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Amenity
-        fields = "__all__"
+        fields = (
+            "pk",
+            "name",
+            "description",
+        )
 
 
 class RoomDetailSerializer(serializers.ModelSerializer):
@@ -30,16 +34,20 @@ class RoomDetailSerializer(serializers.ModelSerializer):
         return room.rating()
 
     def get_is_owner(self, room):
-        request = self.context["request"]
-        return room.owner == request.user
+        request = self.context.get("request")
+        if request:
+            return room.owner == request.user
+        else:
+            return False
 
     def get_is_liked(self, room):
-        request = self.context["request"]
-        if request.user.is_authenticated:
-            return Wishlist.objects.filter(
-                user=request.user,
-                rooms__pk=room.pk,
-            ).exists()
+        request = self.context.get("request")
+        if request:
+            if request.user.is_authenticated:
+                return Wishlist.objects.filter(
+                    user=request.user,
+                    rooms__pk=room.pk,
+                ).exists()
         else:
             return False
 
